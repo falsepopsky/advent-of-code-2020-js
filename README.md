6dd3effe73dc5ac5d8ba2320e901cdefc1bdf984
falsepopsky <javicream@gmail.com>
2020-12-01 22:19:05 -0300

    day 1 part-2 added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Advent Of Code 2020
+# Advent of Code 2020
 
 <div align="center">
   	<img style="margin: 2em 0 2em 0" src="images/AoC-2020.png" alt="AoC Logo"/>
@@ -6,6 +6,10 @@
 </div>
 
 ## My solutions to AoC 2020 in javascript !
+
+```
+run with node !
+```
 
 ### folder structure
 
--- day-1/part-2.js
+++ day-1/part-2.js
@@ -9,25 +9,28 @@ async function sortArray(arrayInputNotSorted) {
   return sortedInput;
 }
 
-async function twoEntriesThatSum2020(inputSorted) {
+async function threeEntriesThatSum2020(inputSorted) {
   for (let i = 0; i < inputLenght; i++) {
     for (let j = 0; j < inputLenght; j++) {
-      if (inputSorted[j] + inputSorted[i] == 2020) {
-        let result = [inputSorted[j], inputSorted[i]];
-        return result;
+      for (let k = 0; k < inputLenght; k++) {
+        if (inputSorted[j] + inputSorted[i] + inputSorted[k] == 2020) {
+          let result = [inputSorted[j], inputSorted[i], inputSorted[k]];
+          return result;
+        }
       }
     }
   }
 }
 
 async function multiplyResult(arrayThatSum2020) {
-  const result = arrayThatSum2020[0] * arrayThatSum2020[1];
+  const result =
+    arrayThatSum2020[0] * arrayThatSum2020[1] * arrayThatSum2020[2];
   return result;
 }
 
 async function main() {
   const sortedNumbers = await sortArray(inputList);
-  const numbersThatSum2020 = await twoEntriesThatSum2020(sortedNumbers);
+  const numbersThatSum2020 = await threeEntriesThatSum2020(sortedNumbers);
   const finalResult = await multiplyResult(numbersThatSum2020);
   console.log(finalResult);
 }
